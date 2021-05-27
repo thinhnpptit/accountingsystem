2f0f5a295138f1e4dcff19b43efa42266d21ec74
hoanghuee <huehth99@gmail.com>
2021-05-28 00:17:48 +0700

    Hoa don ban hang

---
+++ database/migrations/2021_05_26_100521_create_hoa_don_ban_hangs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHoaDonBanHangsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hoa_don_ban_hangs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('bang_mathang');
+            $table->bigInteger('nhanvien_id');
+            $table->float('thanhtien');
+            $table->date('ngay_mua');
+            $table->string('khachhang');
+            $table->bigInteger('phieuthu_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phieu_ban_hangs');
+    }
+}
